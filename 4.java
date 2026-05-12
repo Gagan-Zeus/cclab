6f0d9db6481cf8d5acf779a084a4acf360bdaa53
Gagan Deep S <gaganch17210@gmail.com>
2026-05-12 12:23:49 +0530

    remove

--- 4.java
+++ 4.java
@@ -1,54 +1,40 @@
 package cloudsimm;
-
 import java.text.DecimalFormat;
 import java.util.*;
-
 import org.cloudbus.cloudsim.*;
 import org.cloudbus.cloudsim.core.CloudSim;
 import org.cloudbus.cloudsim.provisioners.*;
 
 public class hgcjgh {
-
     static List<Cloudlet> cl = new ArrayList<>();
     static List<Vm> vm = new ArrayList<>();
 
     public static void main(String[] a) {
         Log.printLine("Starting CloudSimExample2...");
-
         try {
             CloudSim.init(1, Calendar.getInstance(), false);
-
             Datacenter d = createDC("Datacenter_0");
             DatacenterBroker b = new DatacenterBroker("Broker");
-
             int id = b.getId();
-
             vm.add(new Vm(0, id, 250, 1, 512, 1000, 10000, "Xen", new CloudletSchedulerTimeShared()));
             vm.add(new Vm(1, id, 250, 1, 512, 1000, 10000, "Xen", new CloudletSchedulerTimeShared()));
-
             b.submitVmList(vm);
 
             UtilizationModel u = new UtilizationModelFull();
-
             Cloudlet c1 = new Cloudlet(0, 250000, 1, 300, 300, u, u, u);
             c1.setUserId(id);
-
             Cloudlet c2 = new Cloudlet(1, 250000, 1, 300, 300, u, u, u);
             c2.setUserId(id);
-
             cl.add(c1);
             cl.add(c2);
-
             b.submitCloudletList(cl);
 
             b.bindCloudletToVm(0, 0);
             b.bindCloudletToVm(1, 1);
-
             CloudSim.startSimulation();
             CloudSim.stopSimulation();
 
             print(b.getCloudletReceivedList());
-
             d.printDebts();
 
             Log.printLine("CloudSimExample2 finished!");
